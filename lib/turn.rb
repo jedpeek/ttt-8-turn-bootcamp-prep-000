2d13ec5e3f696351e52df3adc8a932e493a78501
Jed Peek <jedpeek@yahoo.com>
2018-05-12 17:46:43 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -43,10 +43,10 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   input_to_index(input)
-  while(valid_move?(board, input))
+  while(valid_move?(board, index))
     "Not a valid move. Enter a number 1-9."
     user_input = gets.strip
     input_to_index(input)
   end
-  move(board, input, value)
+  move(board, index, value)
 end
